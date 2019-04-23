3513e3949a689504e2d9fe1bbd637a87d2478c4d
Lisa Scott <lisarenesoctt@gmail.com>
2019-04-23 18:58:55 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+# co will be built out to contain keys of grade levels.
+#The value of each key will be an array of student names.de here!
+class School
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+    return @roster
+  end
+
+#
+
+end
